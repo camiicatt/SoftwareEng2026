58f2361bb6eba968b6ad6aca726c4f7d63a97f9d
jamibird <lavacube2005@gmail.com>
2026-04-08 23:49:23 -0500

    Make supabase client a singleton to avoid accidental multiple client creation for AccountButton

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
+let supabaseClient: ReturnType<typeof createClient> | null = null;
+
 export function createClientBrowser() {
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  if (!supabaseClient) {
+    supabaseClient = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    );
+  }
+  return supabaseClient;
 }
