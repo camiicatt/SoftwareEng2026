73c37b87abedd85b0d2a10acdaedfe1c042aa8d0
camiicatt <camillehart846@gmail.com>
2026-07-06 20:05:00 -0500

    Configure project for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:5000/:path*",
-      },
-    ];
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
